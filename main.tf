354df30dc393591e0e9cd0795e6bf3bb075c88f3
Andy Seed <pipseed@gmail.com>
2024-08-21 21:59:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,10 +64,6 @@ resource "libvirt_domain" "domain-ubuntu" {
   memory = var.memoryMB
   vcpu = var.vcpu
 
-  cpu { 
-    mode = "host-passthrough"
-  }
-
   disk {
        volume_id = libvirt_volume.disk_ubuntu_resized.id
   }
